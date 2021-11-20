3354965884999580b708dee71f697ac29aeb15f1
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2021-11-20 15:10:54 -0500

    startup and disabled

--- src/main/java/me/joey/jplug/Jplug.java
+++ src/main/java/me/joey/jplug/Jplug.java
@@ -1,17 +1,18 @@
 package me.joey.jplug;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Jplug extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Bukkit.getLogger().info("[JPlug] Jplug has been enabled!");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getLogger().info("[JPlug] Jplug has been disabled!");
     }
 }
