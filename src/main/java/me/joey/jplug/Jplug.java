66e3a82d2cd40d13f7a6444286f28e2071f711f6
joeystub <joey.conover10@gmail.com>
2021-11-20 15:18:56 -0500

    yes commit

--- src/main/java/me/joey/jplug/Jplug.java
+++ src/main/java/me/joey/jplug/Jplug.java
@@ -8,7 +8,7 @@ public final class Jplug extends JavaPlugin {
     @Override
     public void onEnable() {
         Bukkit.getLogger().info("[JPlug] Jplug has been enabled!");
-
+        Bukkit.broadcastMessage("hi i am nigga welcome to my gay server");
     }
 
     @Override
